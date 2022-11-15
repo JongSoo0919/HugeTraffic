45ffe97b2eabab0144dbeafd8687a84573182484
JongSooKim <prjs12@naver.com>
2022-11-15 22:56:37 +0900

    springboot create

---
+++ src/main/java/com/example/trafficproject/TrafficprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.trafficproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trafficproject/TrafficprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trafficproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
